af80eaf362cbd71704c8a8ddb68b5ec838514adc
Philip Seger <seger.philip@gmail.com>
2015-12-09 14:37:57 -0500

    working hamming code

--- hamming_code.py
+++ hamming_code.py
@@ -8,10 +8,10 @@ def cut(msg):
   # check if divisible by 4, if not add buffer 0s to front
   if len(msg) % 4 != 0:
     left = 4 - (len(msg) % 4)
-    sub_msg = left * '0' + msg
+    msg = left * '0' + msg
 
   # slice message into 4 bit chunks
-  chunks = textwrap.wrap(sub_msg, 4)
+  chunks = textwrap.wrap(msg, 4)
 
   # run hamming for each chunk
   for group in chunks:
@@ -19,19 +19,25 @@ def cut(msg):
 
 def hamming(sub_msg):
   # create parity bits for (1,2,3) (2,3,4) (1,3,4)
-  p1 = parity( ,(0, 1, 2))
-  p2 = parity( ,(1, 2, 3))
-  p3 = parity( ,(0, 2, 3))
+  p1 = parity(sub_msg, (0, 1, 2))
+  p2 = parity(sub_msg, (1, 2, 3))
+  p3 = parity(sub_msg, (0, 2, 3))
 
   # need to get 3 parity bits using above, then add them to message
+  print sub_msg + p1 + p2 + p3
   return sub_msg + p1 + p2 + p3
 
-def parity():
+def parity(sub_msg, indices):
   # apply the matrix for general parity
-  pass
+  par = ''
+  for i in indices:
+    par += sub_msg[i]
+
+  return str(str.count(par, '1') % 2)
 
 # TODO: implement decoding of hamming (reverse process + checking)
 
 if __name__ == '__main__':
   input_msg = '011100000110100101110000'
+  # input_msg = '0111'
   cut(input_msg)
